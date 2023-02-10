89f31a55413334ff747cbd553377ec393d95688a
MimiTest2 <91503103+MimiTest2@users.noreply.github.com>
2023-02-09 20:14:47 -0500

    Update Example.lua

--- Example.lua
+++ Example.lua
@@ -1,5 +1,20 @@
-  local lol = require(game.ReplicatedStorage.ModuleScript); --replace the require modulescript wtih a loadstring to esp library.lua
-	lol.initmodule(false)
-	local lol2 = lol.init("esptest",false);
-	local part = workspace.Trinkets.Part;
-	lol.createesp({PrimaryPart = part, Name = "trinket", Color = Color3.fromRGB(65, 255, 160), Transparency = 0.75, UseChams = true}, part, lol2)
+--lib is a loadstring of https://raw.githubusercontent.com/MimiTest2/EspLibrary/main/Esp%20Library.lua. shouldnt use this if u dont know what ur doing    
+
+    lib.initmodule(false)
+    local lol2 = lib.init("esp",false);
+    task.spawn(function()
+    	while true do
+    		wait(0.5)
+    		lol2.Enabled = false;
+    		wait(0.5)
+    		lol2.Enabled = true;
+    	end
+    end)
+    while task.wait(1) do
+        for i, v in pairs(game.Players:GetPlayers()) do
+            if not lib.find(v.Character.Name) then
+                local part = v.Character;
+                lib.createesp({PrimaryPart = part.PrimaryPart, Name = v.Name,  Font = Enum.Font.Code, Size = UDim2.new(0, 100, 0, 100), TextSize = 14, Color = Color3.fromRGB(65, 255, 160), Transparency = 0.75, UseChams = true}, part, lol2)
+            end
+        end
+    end
